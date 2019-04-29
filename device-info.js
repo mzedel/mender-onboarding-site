0576e890e8a6f1ae2ee57fbe499f980e05f79178
Manuel Zedel <manuel.zedel@northern.tech>
2019-04-29 14:25:43 +0200

    onboarding site, showing some device specific infos added
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

---
+++ device-info.js
@@ -0,0 +1,15 @@
+  mender_server = {
+    "Web server": "2f687077256b",
+    "Server address": "172.18.1.2"
+  }
+  mender_identity = {
+    "Device ID": "",
+    "mac": "12:34:ac:14:24:02"
+  }
+  mender_inventory = {
+    "device_type": "test device",
+    "mender_client_version": "2.0.0-beta",
+    "os": "Linux version 4.9.125-linuxkit (root@659b6d51c354) (gcc version 6.4.0 (Alpine 6.4.0) ) #1 SMP Fri Sep 7 08:20:28 UTC 2018",
+    "cpu": "Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz",
+    "kernel": "4.9.125-linuxkit"
+  }
